0cefe926ba3020fcba708e178716290172706721
Adriana McGee <adriana@Adrianas-MBP.lan>
2022-06-09 00:50:36 -0400

    commit tree 7051fd0ec085f18c196a36f05d458483d05171f6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainFrame = document.getElementById('main');
+mainFrame.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Adriana is the champion';
